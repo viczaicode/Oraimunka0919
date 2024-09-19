4f665f4ee706d35588a00b4fdd9a2b4d513dcdbe
viczaicode <viczaimilan2003@gmail.com>
2024-09-19 16:35:24 +0200

    Clean Coding, 4. feladat elvégezve

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 import metodusok
 
+#####################################
+# BEKÉRÉS // MINDEN FELADATHOZ KELL #
+#####################################
 bekerendoszam = metodusok.szambekeres()
+
+#####################################
+#         // 1. FELADAT //          #
+#####################################
+
 pozitivE = metodusok.pozitivE(bekerendoszam)
+
+#####################################
+#         // 2. FELADAT //          #
+#####################################
+
 egyjegyuEszam = metodusok.egyjegyuE(bekerendoszam)
 kisebbszomszed, nagyobbszomszed = metodusok.szomszedszamok(egyjegyuEszam)
+
+#####################################
+#         // 3. FELADAT //          #
+#####################################
+
 szazalekososztalyzat = metodusok.szazalekososztalyzat(bekerendoszam)
 
-metodusok.globaliskiiras(pozitivE, egyjegyuEszam, kisebbszomszed, nagyobbszomszed, szazalekososztalyzat)
+#####################################
+#         // 4. FELADAT //          #
+#####################################
+
+egeszszamosztalyzat = metodusok.egeszszamososztalyzat(bekerendoszam)
+
+#####################################
+#      // GLOBÁLIS KIÍRÁS //        #
+#      MINDIG AZ ALJÁN LEGYEN       #
+#####################################
+
+metodusok.globaliskiiras(pozitivE, egyjegyuEszam, kisebbszomszed, nagyobbszomszed, szazalekososztalyzat, egeszszamosztalyzat)
--- metodusok.py
+++ metodusok.py
@@ -33,10 +33,33 @@ def szazalekososztalyzat(szazalekoseredmeny):
             return "Jeles"
         else:
             return "Hiba: érvénytelen százalék!"
-
-def globaliskiiras(pozitivEReturn:str,egyjegyuEszam:int,kisebbszomszed:int, nagyobbszomszed:int, szazalekososztalyzat):
+        
+def egeszszamososztalyzat(egeszSzamOsztalyzat):
+    if (egeszSzamOsztalyzat == 1):
+        return egeszSzamOsztalyzat
+    elif (egeszSzamOsztalyzat == 2):
+        return egeszSzamOsztalyzat
+    elif (egeszSzamOsztalyzat == 3):
+        return egeszSzamOsztalyzat
+    elif (egeszSzamOsztalyzat == 4):
+        return egeszSzamOsztalyzat
+    elif (egeszSzamOsztalyzat == 5):
+        return egeszSzamOsztalyzat
+    
+def globaliskiiras(pozitivEReturn:str,egyjegyuEszam:int,kisebbszomszed:int, nagyobbszomszed:int, szazalekososztalyzat:int, egeszSzamOsztalyzat:int):
     if not (pozitivEReturn == ""):
         print(pozitivEReturn)
     if (egyjegyuEszam != 0):
         print(f"Mivel a megadott szám egyjegyű volt, ezért kiírom az egyel kisebb, és nagyobb szomszédjait: {kisebbszomszed}, {nagyobbszomszed}")
     print(f"Az elért osztályzat: {szazalekososztalyzat}")
+
+    if (egeszSzamOsztalyzat == 1):
+        print(f"Az egész számos osztályzat eredménye: Elégtelen ({egeszSzamOsztalyzat})")
+    elif (egeszSzamOsztalyzat == 2):
+        print(f"Az egész számos osztályzat eredménye: Elégséges ({egeszSzamOsztalyzat})")
+    elif (egeszSzamOsztalyzat == 3):
+        print(f"Az egész számos osztályzat eredménye: Közepes ({egeszSzamOsztalyzat})")
+    elif (egeszSzamOsztalyzat == 4):
+        print(f"Az egész számos osztályzat eredménye: Jó ({egeszSzamOsztalyzat})")
+    elif (egeszSzamOsztalyzat == 5):
+        print(f"Az egész számos osztályzat eredménye: Jeles ({egeszSzamOsztalyzat})")
